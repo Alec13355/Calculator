bb1331ab2b6806136f4f015d6df43d55ec13caee
Alec Harrison <alech@iastate.edu>
2017-01-11 16:39:14 -0600

    Start of a Calculator

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Alec Harrison on 1/11/17.
+//  Copyright © 2017 Alec Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
